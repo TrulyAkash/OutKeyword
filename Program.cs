cb378fedcf2ee7164c5971bb5d914a9b04115fb6
Akash Kumar Sengar <akashsengar55@gmail.com>
2022-03-15 05:18:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class TestOUT
+{
+ 
+    public static void Addition(out int p, ref int q)
+    {
+        
+        p = 30;
+        q = 40;
+        p += p;
+        q += q;
+    }
+
+   
+    static public void Main()
+    {
+
+        
+        int i, j=10;           
+
+        
+        Addition(out i,ref j); 
+
+        
+        Console.WriteLine("The addition of the value is: {0}", i);
+        Console.WriteLine("The addition of the value is: {0}", j);
+    }
+
+    
+}
